2c8da451bacef5d50e48a3b81b4fd7d63058e146
Roman Suvorov <suvorov83@gmail.com>
2022-02-08 02:38:56 +0300

    subscribe on stream-added and stream-removed

--- extension.js
+++ extension.js
@@ -7,33 +7,45 @@ class AccessIndicator extends St.Widget {
         _init() {
             super._init({ name: 'Access indicator', reactive: true });
             
-            // devices to observe
-            this.mic = 0;
-            this.camera = 0;
-
             // indicator
             this.indicator = Main.panel.statusArea.dateMenu._indicator;
             this.originalStyle = this.indicator.style;
             this.customStyle = 'color: #3ade85;';
-            
-            this.indicator.style = this.customStyle
-            this.indicator.visible = true;
             // // sound mixer
             this.mixer_control = new Gvc.MixerControl({ name: "sound access indicator" });
+            this.mixer_control.connect('stream-added', this._show_access_indicator.bind(this));
+            this.mixer_control.connect('stream-removed', this._hide_access_indicator.bind(this));
             this.mixer_control.open();
-            this.mixer_control.connect('default-source-changed', this._mic_state_changed.bind(this));
-            this.mixer_control.connect('stream-added', this._mic_state_changed.bind(this));
-            this.mixer_control.connect('stream-removed', this._mic_state_changed.bind(this));
-            this.mixer_control.connect("state-changed", this._mic_state_changed);
-            this.stream = this.mixer_control.get_default_source();
+            this.active = false;
+
         }
 
         _onDestroy() {
             super._onDestroy();
         }
 
-        _mic_state_changed(state) {
-            log(state)
+        _show_access_indicator(obj) {
+            log(obj)
+            this.active = true;
+            this.indicator.style = this.customStyle;
+            this.indicator.visible = true;
+        }
+
+        _hide_access_indicator(obj) {
+            log(obj)
+            this.active = false;
+            this.indicator.style = this.originalStyle;
+
+            // don't just set indicator visible = false, but call
+            // parent._updateCount in order give it to make a decision
+            // whether the notification indicator must be shown or not
+            this.indicator._updateCount();
+        }
+
+        _updateCount() {
+            if (!this.active) {
+                this.indicator._updateCount();
+            }
         }
 });
 
@@ -50,54 +62,6 @@ class Extension {
         this._indicator = null;
     }
 }
-
-// class Extension {
-//     constructor(uuid) {
-//         this._uuid = uuid;
-        
-//         // devices to observe
-//         this.mic = 0;
-//         this.camera = 0;
-
-//         // indicator
-//         this.indicator = Main.panel.statusArea.dateMenu._indicator;
-//         this.originalStyle = this.indicator.style;
-//         this.customStyle = 'color: #3ade85;';
-//         this.indicator.style = this.customStyle
-//         this.indicator.visible = true;
-
-//         // // sound mixer
-//         // this.mixer_control = new Gvc.MixerControl({ name: "sound access indicator" });
-//         // this.mixer_control.open();
-//         // this.mixer_control.connect("state-changed", this._mic_state_changed);
-//     }
-
-//     // _mic_state_changed(this, state) {
-//         // this.mic = state
-//         // this._updateCount()
-//     // }
-
-//     _updateCount() {
-//         // let count = this.camera + this.mic;
-//         // if (count > 0) {
-//         //     this.indicator._count += count;
-//         //     this.indicator.style = this.customStyle;
-//         //     this.indicator.visible = true
-//         // } else {
-//         //     this.indicator.style = this.originalStyle;
-//         //     this.indicator._updateCount();
-//         // }
-//     }
-
-//     enable() {
-//         this._updateCount();
-//     }
-
-//     destroy() {
-//         this.indicator.style = this.originalStyle;
-//         this.indicator._sync();
-//     }
-// }
 
 function init() {
     return new Extension();
